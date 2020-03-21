8e30a1f101f8ac8e81729cde7205a954546185c5
wf <wangfeng@guoji.biz>
2020-03-21 17:25:31 +0800

    1

--- service/src/config/index.js
+++ service/src/config/index.js
@@ -3,5 +3,19 @@ const isDevMode = Object.is(environment, 'development');
 const isProdMode = Object.is(environment, 'production');
 
 module.exports = {
-  port: isDevMode? 3000 : 3030
+  port: isDevMode? 3000 : 3030,
+
+  db: {
+    // 主机名称，一般是本机
+    host: 'localhost',
+    // 数据库的端口号，如果不设置，默认是3306
+    port: '3306',
+    // 创建数据库时设置用户名
+    user: 'root',
+    // 创建数据库时设置的密码
+    password: '123456',
+    // 创建的数据库
+    database: 'myBlog'
+  }
+
 }
